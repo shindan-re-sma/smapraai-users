b4f749a73bb55e9ef5a10d0ea59c00833bef1551
nakazono1011 <syousanitiosi1011@yahoo.co.jp>
2021-03-12 22:39:43 +0900

    update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
-import * as functions from "firebase-functions";
+// import * as functions from "firebase-functions";
+console.log("done");
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
